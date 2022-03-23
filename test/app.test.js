c610fc2c2b72ceadde2efa3498c7e830228b3b05
teri0411 <goaheadman@naver.com>
2022-03-23 14:16:39 +0900

    워크플로우 생성전

--- test/app.test.js
+++ test/app.test.js
@@ -10,15 +10,15 @@ describe('유닛 테스트 101', () => {
   })
 
   it('결과에 대한 기대값(expectation value)를 비교하여 유닛 테스트를 할 수 있습니다', () => {
-    expect(1 + 1).to.be.equal(FILL_ME_IN)
-    expect(100 + 200).to.be.equal(FILL_ME_IN)
+    expect(1 + 1).to.be.equal(2)
+    expect(100 + 200).to.be.equal(300)
   })
 
   it('서버에 GET / 요청을 보내면 Hello World!라는 텍스트가 응답으로 옵니다', () => {
     return request(app)
       .get('/')
       .then(res => {
-        expect(res.text).to.be.equal(FILL_ME_IN)
+        expect(res.text).to.be.equal('Hello World!')
       })
   })
 
@@ -28,17 +28,17 @@ describe('유닛 테스트 101', () => {
       .send('"coDeStaTes"')
       .set('Content-Type', 'application/json')
       .then(res => {
-        expect(res.body).to.be.equal(FILL_ME_IN)
+        expect(res.body).to.be.equal('CODESTATES')
       })
   })
 
-  it('서버에 POST /lower 요청에 body를 실어 보내면 응답은 대문자로 돌려줍니다', () => {
+  it('서버에 POST /lower 요청에 body를 실어 보내면 응답은 소문자로 돌려줍니다', () => {
     return request(app)
       .post('/lower')
       .send('"coDeStaTes"')
       .set('Content-Type', 'application/json')
       .then(res => {
-        expect(res.body).to.be.equal(FILL_ME_IN)
+        expect(res.body).to.be.equal('codestates')
       })
   })
 })
